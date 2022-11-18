1c214e49db4d32d0e0c1bd115698e527a28e05e9
abdessamadbettal <abdessamadbettal@gmail.com>
2022-11-18 19:17:47 +0100

    animation

--- frentend/src/pages/Home.js
+++ frentend/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import Banner from '../components/banner/Banner'
+import SpecialiteCategory from '../components/home/SpecialiteCategory'
 
 const Home = () => {
   return (
+    <>
         <Banner/>
+        <SpecialiteCategory />
+    </>
   )
 }
 
